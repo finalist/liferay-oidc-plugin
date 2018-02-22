58fcb72834050c24e6a9378d4f1d00a95c491dcf
Geert van der Ploeg <geert.van.der.ploeg@finalist.nl>
2018-02-22 13:52:19 +0100

    Issue #22, fixed compatibility from CE GA4 onwards, and DXP+.

--- dxp-oidc-portalsettings-web/src/main/java/nl/finalist/liferay/oidc/settings/internal/portlet/action/OidcPortalSettingsFormContributor.java
+++ dxp-oidc-portalsettings-web/src/main/java/nl/finalist/liferay/oidc/settings/internal/portlet/action/OidcPortalSettingsFormContributor.java
@@ -1,12 +1,11 @@
 package nl.finalist.liferay.oidc.settings.internal.portlet.action;
 
-import com.liferay.portal.settings.web.portlet.action.PortalSettingsFormContributor;
-
-import java.util.Optional;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.settings.portlet.action.BasePortalSettingsFormMVCActionCommand;
+import com.liferay.portal.settings.web.constants.PortalSettingsPortletKeys;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.PortletException;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -17,34 +16,29 @@ import nl.finalist.liferay.oidc.settings.internal.constants.PortalSettingsOidcCo
  * Form contributor for the Virtual Instance's 'Instance Settings'.
  * To be used together with the OCD class and its *BeanDeclaration and *PidMapping.
  */
-@Component(immediate = true, service = PortalSettingsFormContributor.class)
-public class OidcPortalSettingsFormContributor implements PortalSettingsFormContributor {
+@Component(immediate = true,
+    property = {
+        "javax.portlet.name=" + PortalSettingsPortletKeys.PORTAL_SETTINGS,
+        "mvc.command.name=/portal_settings/oidc"
+    },
+    service = MVCActionCommand.class)
+public class OidcPortalSettingsFormContributor extends BasePortalSettingsFormMVCActionCommand {
+
 
     @Override
-    public Optional<String> getDeleteMVCActionCommandNameOptional() {
-        return Optional.of("/portal_settings/oidc_delete");
+    protected void doValidateForm(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
     }
 
     @Override
-    public String getParameterNamespace() {
+	protected String getParameterNamespace() {
         return PortalSettingsOidcConstants.FORM_PARAMETER_NAMESPACE;
     }
 
-    @Override
-    public Optional<String> getSaveMVCActionCommandNameOptional() {
-        return Optional.of("/portal_settings/oidc");
-    }
 
     @Override
     public String getSettingsId() {
         return OpenIDConnectOCDConfigurationConstants.SERVICE_NAME;
     }
 
-    @Override
-    public void validateForm(
-        ActionRequest actionRequest, ActionResponse actionResponse)
-        throws PortletException {
-        // No specific validation.
-    }
 
 }
